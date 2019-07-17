d070596077fc3ef564127462cf2024b8c7e1cd7c
6at9l <6at9lodi@gmail.com>
2019-07-17 21:41:29 +0300

    test
    test

---
+++ precache-manifest.a2ba320639497f64202cf1d863bcb208.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bd5b7baf39235b2c09048e25ae3dea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c692703bd335dc8a3666",
+    "url": "/static/css/main.c508bb44.chunk.css"
+  },
+  {
+    "revision": "0e87bf7fc4bc43563bd5",
+    "url": "/static/js/2.b0c62877.chunk.js"
+  },
+  {
+    "revision": "c692703bd335dc8a3666",
+    "url": "/static/js/main.aad91ddf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
